df86a63b1de5dbe4c2e59de0a875efbbf91ac879
Yorghethy18 <yorghetyauri123@gmail.com>
2024-01-18 21:46:40 -0500

    selección sub y distrito hecho

--- js.php
+++ js.php
@@ -301,8 +301,10 @@
           .then(respuesta => respuesta.json())
           .then(datos => {
             console.log(datos);
+
             const subcategoriaDiv = document.getElementById("subcategoria");
             datos.forEach(element => {
+
               // Mostrar categoría
               const nuevaFilaCategoria = `
                 <div id="${element.categoria}" class="pb-5 w-820 text-center nego_acti" style="display:none;" data-id="${element.categoria}">
@@ -352,70 +354,70 @@
             method: "POST",
             body: parametros
           })
-          .then(respuesta => respuesta.json())
-          .then(datos =>{
-            //console.log(datos)
-            const distritoSelect = document.getElementById("distrito");
-            datos.forEach(element => {
-              const etiqueta = document.createElement("option");
-              etiqueta.value = element.iddistrito;
-              etiqueta.innerHTML = element.nomdistrito;
-              distritoSelect.appendChild(etiqueta);
-            });
-          })
-          .catch(e => {
-            console.error(e);
-          });
-        }
-      
-        function actualizarMapa() {
+        .then(respuesta => respuesta.json())
+        .then(datos =>{
+          //console.log(datos)
           const distritoSelect = document.getElementById("distrito");
-          const selectedDistritoId = distritoSelect.value;
-
-          //clearMarkers();
-          if (selectedDistritoId) {
-            // Realiza una llamada para obtener las coordenadas del distrito seleccionado
-            obtenerCoordenadasDistrito(selectedDistritoId)
-            .then(coordenadas => {
-              // Actualiza el mapa con las nuevas coordenadas
-              map.setCenter(coordenadas);
-              map.setZoom(16);
-
-              // Agrega un marcador en las nuevas coordenadas
-              const marcadorDistrito = agregarMarcadorDistrito(selectedDistritoId, coordenadas[0], coordenadas[1]);
-            })
-            .catch(error => {
-              console.error("Error al obtener coordenadas del distrito:", error);
-            });
-          }
-        }
-
-        document.addEventListener('change', function (event) {
-          if (event.target.id === 'distrito') {
-            actualizarMapa();
-          }
+          datos.forEach(element => {
+            const etiqueta = document.createElement("option");
+            etiqueta.value = element.iddistrito;
+            etiqueta.innerHTML = element.nomdistrito;
+            distritoSelect.appendChild(etiqueta);
+          });
+        })
+        .catch(e => {
+          console.error(e);
         });
+      }
       
-      
-        // Función para obtener las coordenadas del distrito desde el servidor
-        function obtenerCoordenadasDistrito(distritoId) {
-          const parametros = new FormData();
-          parametros.append("operacion", "obtener");
-          parametros.append("iddistrito", distritoId);
-  
-          return fetch(`./controllers/distrito.controller.php`, {
-            method: "POST",
-            body: parametros
-          })
-          .then(respuesta => respuesta.json())
-          .then(coordenadas => ({
-            lat: parseFloat(coordenadas.latitud),
-            lng: parseFloat(coordenadas.longitud)
-          }));
+      function actualizarMapa() {
+  const distritoSelect = document.getElementById("distrito");
+  const selectedDistritoId = distritoSelect.value;
+
+  //clearMarkers();
+  if (selectedDistritoId) {
+    // Realiza una llamada para obtener las coordenadas del distrito seleccionado
+    obtenerCoordenadasDistrito(selectedDistritoId)
+    .then(coordenadas => {
+      // Actualiza el mapa con las nuevas coordenadas
+      map.setCenter(coordenadas);
+      map.setZoom(16);
+
+      // Agrega un marcador en las nuevas coordenadas
+      const marcadorDistrito = agregarMarcadorDistrito(selectedDistritoId, coordenadas[0], coordenadas[1]);
+    })
+    .catch(error => {
+      console.error("Error al obtener coordenadas del distrito:", error);
+    });
+  }
+}
+
+      document.addEventListener('change', function (event) {
+        if (event.target.id === 'distrito') {
+          actualizarMapa();
         }
+      });
+      
+      
+      // Función para obtener las coordenadas del distrito desde el servidor
+      function obtenerCoordenadasDistrito(distritoId) {
+        const parametros = new FormData();
+        parametros.append("operacion", "obtener");
+        parametros.append("iddistrito", distritoId);
+  
+        return fetch(`./controllers/distrito.controller.php`, {
+          method: "POST",
+          body: parametros
+        })
+        .then(respuesta => respuesta.json())
+        .then(coordenadas => ({
+          lat: parseFloat(coordenadas.latitud),
+          lng: parseFloat(coordenadas.longitud)
+        }));
+      }
 
       
-        function carrusel() {
+      function carrusel() {
           const parametros = new FormData();
           parametros.append("operacion", "listar");
 
@@ -441,7 +443,7 @@
 
             const owlStage = document.createElement("div");
             owlStage.className = "owl-stage";
-            owlStage.style = "transform: translate3d(0px, 0px, 0px); transition: all 1s ease 0s; width: 10000%; overflow: hidden;";  //     Ajusta el valor de width según sea necesario
+            owlStage.style = "transform: translate3d(0px, 0px, 0px); transition: all 1s ease 0s; width: 10000%; overflow: hidden;";  // Ajusta el valor de width según sea necesario
 
             datos.forEach(element => {
               const owlItem = document.createElement("div");
@@ -503,7 +505,7 @@
             const owlNav = document.createElement("div");
             owlNav.className = "owl-nav";
             owlNav.innerHTML = '<button type="button" role="presentation" class="owl-prev"><span aria-label="Previous">‹</span></button>' +
-            '<button type="button" role="presentation" class="owl-next"><span aria-label="Next">›</span></button>';
+              '<button type="button" role="presentation" class="owl-next"><span aria-label="Next">›</span></button>';
 
             const owlDots = document.createElement("div");
             owlDots.className = "owl-dots";
@@ -515,201 +517,202 @@
           .catch(e => {
             console.error(e);
           });
-        }
-      
-        carrusel();
-        // Llamar a la función para obtener categorías al cargar la página
-        getCategoria();
-        cargarSubcategorias();
-        getDistrito();
-      }); //FIN DEL DOM
+       }
+       carrusel();
+      // Llamar a la función para obtener categorías al cargar la página
+      getCategoria();
+      cargarSubcategorias();
+      getDistrito();
+    }); //FIN DEL DOM
 
-      // FUNCIONES PARA EL MAPA
-      function initMap() {
-        const peruCoords = {
-          lat: -13.4098500,
-          lng: -76.1323500
-        };
-        const mapDiv = document.getElementById('mapDiv');
-        map = new google.maps.Map(mapDiv, {
-          center: peruCoords,
-          zoom: 16,
-        });
-        // Inicializar el objeto infoWindow
-        infoWindow = new google.maps.InfoWindow();
-        getYourLocation();
-      }
+    // FUNCIONES PARA EL MAPA
+    function initMap() {
+      const peruCoords = {
+        lat: -13.4098500,
+        lng: -76.1323500
+      };
+      const mapDiv = document.getElementById('mapDiv');
+      map = new google.maps.Map(mapDiv, {
+        center: peruCoords,
+        zoom: 16,
+      });
+      // Inicializar el objeto infoWindow
+      infoWindow = new google.maps.InfoWindow();
+      getYourLocation();
+    }
 
-      // Declarar una variable global para almacenar los marcadores
-      let markers = [];
-      let infoWindow;
+    // Declarar una variable global para almacenar los marcadores
+    let markers = [];
+    let infoWindow;
 
-      function clearMarkers() {
-        markers.forEach(marker => {
-          marker.setMap(null);
-        });
-        markers = [];
-      } 
+    function clearMarkers() {
+      markers.forEach(marker => {
+        marker.setMap(null);
+      });
+      markers = [];
+    }
 
     
 
-      document.addEventListener('change', function (event) {
-        if (event.target.id === 'distrito') {
-          // Obtener el ID de la subcategoría seleccionada
-          console.log(document.querySelector('.nego_acti'));
-          const subcategoriaSeleccionada = document.querySelector('.nego_acti').getAttribute('data-idsubcategoria');
+    document.addEventListener('change', function (event) {
+      if (event.target.id === 'distrito') {
+        // Obtener el ID de la subcategoría seleccionada
+        console.log(document.querySelector('.nego_acti'));
+        const subcategoriaSeleccionada = document.querySelector('.nego_acti').getAttribute('data-idsubcategoria');
 
-          // Obtener el ID del distrito seleccionado
-          const distritoSeleccionado = event.target.value;
+        // Obtener el ID del distrito seleccionado
+        const distritoSeleccionado = event.target.value;
 
-          // Verificar si se seleccionó una subcategoría y un distrito
-          if (subcategoriaSeleccionada && distritoSeleccionado) {
-            // Llamar a la función para listar negocios
-            listarNegocios(subcategoriaSeleccionada, distritoSeleccionado);
-          }
+        // Verificar si se seleccionó una subcategoría y un distrito
+        if (subcategoriaSeleccionada && distritoSeleccionado) {
+          // Llamar a la función para listar negocios
+          listarNegocios(subcategoriaSeleccionada, distritoSeleccionado);
         }
-      });
-
-      function listarNegocios(idsubcategoria, iddistrito) {
-        console.log("Ingresando a listarNegocios");
-        // Limpiar marcadores existentes
-        clearMarkers();
-        // Limpiar el select de distritos
-        const distritoSelect = document.getElementById("distrito");
-        distritoSelect.selectedIndex = 0;
-
-        if (iddistrito === distritoSelect.value) {
-          return;
-        }
-        const parametros = new FormData();
-        parametros.append("operacion", "obtenerNyH");
-        parametros.append("idsubcategoria", idsubcategoria);
-        parametros.append("iddistrito", iddistrito);
-
-        fetch('./controllers/negocio.controller.php', {
-          method: "POST",
-          body: parametros
-        })
-        .then(respuesta => respuesta.json())
-        .then(datos => {
-          console.log(datos);
-          if (datos.length > 0) {
-            // Crear un bucle para recorrer todos los elementos y agregar marcadores
-            datos.forEach(element => {
-              const point = new google.maps.LatLng(
-                parseFloat(element.latitud),
-                parseFloat(element.longitud)
-              );
-              // Agregar un marcador para cada elemento
-              const marker = new google.maps.Marker({
-                map: map, // Asumo que "map" es tu objeto google.maps.Map
-                position: point,
-                title: element.nombre,
-              });
-              marker.addListener('mouseover', function () {
-                mostrarInfoWindow(element, marker);
-              });
-              marker.addListener('mouseout', function () {
-                infoWindow.close();
-              });
-              markers.push(marker);
-            });
-            // Centrar y hacer zoom solo si hay marcadores
-            if (markers.length > 0) {
-              const bounds = new google.maps.LatLngBounds();
-              markers.forEach(marker => {
-                bounds.extend(marker.getPosition());
-              });
-              map.fitBounds(bounds);
-            }
-          }
-        })
-        .catch(e => {
-          console.error(e);
-        });
       }
-      
-      function agregarMarcadorDistrito(iddistrito, latitud, longitud) {
+    });
+
+    function listarNegocios(idsubcategoria, iddistrito) {
+  console.log("Ingresando a listarNegocios");
+  // Limpiar marcadores existentes
+  clearMarkers();
+  // Limpiar el select de distritos
+  const distritoSelect = document.getElementById("distrito");
+  distritoSelect.selectedIndex = 0;
+
+  if (iddistrito === distritoSelect.value) {
+    return;
+  }
+
+  const parametros = new FormData();
+  parametros.append("operacion", "obtenerNyH");
+  parametros.append("idsubcategoria", idsubcategoria);
+  parametros.append("iddistrito", iddistrito);
+
+  fetch('./controllers/negocio.controller.php', {
+    method: "POST",
+    body: parametros
+  })
+  .then(respuesta => respuesta.json())
+  .then(datos => {
+    console.log(datos);
+    if (datos.length > 0) {
+      // Crear un bucle para recorrer todos los elementos y agregar marcadores
+      datos.forEach(element => {
+        const point = new google.maps.LatLng(
+          parseFloat(element.latitud),
+          parseFloat(element.longitud)
+        );
+        // Agregar un marcador para cada elemento
         const marker = new google.maps.Marker({
-          position: new google.maps.LatLng(latitud, longitud),
-          map: map,
-          icon: "./img/ubicacion.svg"
+          map: map, // Asumo que "map" es tu objeto google.maps.Map
+          position: point,
+          title: element.nombre,
         });
-        return marker;
-      }
-
-      // Evento de clic en los botones de subcategoría
-      document.addEventListener('click', function (event) {
-        if (event.target.classList.contains('btn-light')) {
-          const idSubcategoria = obtenerIdSubcategoriaDesdeBoton(event.target);
-          if (idSubcategoria !== null) {
-            console.log("ID de Subcategoría:", idSubcategoria);
-            listarNegocios(idSubcategoria);
-          }
-        }
+        marker.addListener('mouseover', function () {
+          mostrarInfoWindow(element, marker);
+        });
+        marker.addListener('mouseout', function () {
+          infoWindow.close();
+        });
+        markers.push(marker);
       });
+      // Centrar y hacer zoom solo si hay marcadores
+      if (markers.length > 0) {
+        const bounds = new google.maps.LatLngBounds();
+        markers.forEach(marker => {
+          bounds.extend(marker.getPosition());
+        });
+        map.fitBounds(bounds);
+      }
+    }
+  })
+  .catch(e => {
+    console.error(e);
+  });
+}
+function agregarMarcadorDistrito(iddistrito, latitud, longitud) {
+  const marker = new google.maps.Marker({
+    position: new google.maps.LatLng(latitud, longitud),
+    map: map,
+    icon: "./img/ubicacion.svg"
+  });
+  return marker;
+}
+
+
     
-      // Función para obtener el ID de subcategoría desde el botón
-      function obtenerIdSubcategoriaDesdeBoton(boton) {
-        // Verificar si el atributo 'data-idsubcategoria' está presente
-        if (boton && boton.getAttribute) {
-          // Obtener el valor del atributo 'data-idsubcategoria'
-          const idSubcategoria = parseInt(boton.getAttribute('data-idsubcategoria'));
-          if (!isNaN(idSubcategoria)) {
-            return idSubcategoria;
-          } else {
-            console.error("El botón no tiene un valor válido para 'data-idsubcategoria'.");
-            return null;
-          }
+    // Evento de clic en los botones de subcategoría
+    document.addEventListener('click', function (event) {
+      if (event.target.classList.contains('btn-light')) {
+        const idSubcategoria = obtenerIdSubcategoriaDesdeBoton(event.target);
+        if (idSubcategoria !== null) {
+          console.log("ID de Subcategoría:", idSubcategoria);
+          listarNegocios(idSubcategoria);
+        }
+      }
+    });
+    
+    // Función para obtener el ID de subcategoría desde el botón
+    function obtenerIdSubcategoriaDesdeBoton(boton) {
+      // Verificar si el atributo 'data-idsubcategoria' está presente
+      if (boton && boton.getAttribute) {
+        // Obtener el valor del atributo 'data-idsubcategoria'
+        const idSubcategoria = parseInt(boton.getAttribute('data-idsubcategoria'));
+        if (!isNaN(idSubcategoria)) {
+          return idSubcategoria;
         } else {
-          console.error("El botón no tiene el atributo 'data-idsubcategoria' definido.");
+          console.error("El botón no tiene un valor válido para 'data-idsubcategoria'.");
           return null;
         }
+      } else {
+        console.error("El botón no tiene el atributo 'data-idsubcategoria' definido.");
+        return null;
       }
+    }
 
-      function mostrarInfoWindow(element, marker) {
-        const contentString = `
-          <div class="card-window">
-            <div class="logo-window">
-              <img src="./img/Donald-Trump-sign-in-snow-Urbandale-IA-Jan.-13-2024.webp" alt="Logo de la chifa oriental" style="width: 100px; height: 100px; object-fit: cover;">
-            </div>
-            <div class="info-window">
-              <h1 class="name-window">${element.nombre}</h1>
-              <h2 class="title-window">${element.Estado}</h2>
-              <p class="distrito-window">${element.nomdistrito}</p>
-              <p class="phone-window"><i class="bi bi-whatsapp"></i> ${element.telefono}</p>
-            </div>
-          </div>
-        `;
-        infoWindow.setContent(contentString);
-        infoWindow.open(map, marker);
-      }
+    function mostrarInfoWindow(element, marker) {
+      const contentString = `
+      <div class="card-window">
+        <div class="logo-window">
+          <img src="./img/Donald-Trump-sign-in-snow-Urbandale-IA-Jan.-13-2024.webp" alt="Logo de la chifa oriental" style="width: 100px; height: 100px; object-fit: cover;">
+        </div>
+        <div class="info-window">
+          <h1 class="name-window">${element.nombre}</h1>
+          <h2 class="title-window">${element.Estado}</h2>
+          <p class="distrito-window">${element.nomdistrito}</p>
+          <p class="phone-window"><i class="bi bi-whatsapp"></i> ${element.telefono}</p>
+        </div>
+      </div>`;
+      infoWindow.setContent(contentString);
+      infoWindow.open(map, marker);
+    }
     
-      function getYourLocation() {
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(
-            (position) => {
-              const coords = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-              };
-              map.setCenter(coords);
-              map.setZoom(16);
+    function getYourLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const coords = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
 
-              new google.maps.Marker({
-                position: coords,
-                map: map,
-                icon: "./img/ubicacion.svg"
-              });
-            },
-            () => {
-              alert("Tu navegador está bien, pero ocurrió un error al obtener tu ubicación");
-            }
-          );
-        } else {
-          alert("Tu navegador no cuenta con geolocalización");
-        }
-      } 
+            map.setCenter(coords);
+            map.setZoom(16);
+
+            new google.maps.Marker({
+              position: coords,
+              map: map,
+              icon: "./img/ubicacion.svg"
+            });
+          },
+          () => {
+            alert("Tu navegador está bien, pero ocurrió un error al obtener tu ubicación");
+          }
+        );
+      } else {
+        alert("Tu navegador no cuenta con geolocalización");
+      }
+    } 
     </script>
   </body>
 </html>
